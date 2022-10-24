98998e7f790017fc0eb8d4eb72a8ea5c9dc96919
Matsashi <matthew.pottier@hotmail.fr>
2022-10-24 12:29:55 +0200

    integration jfiglet

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,11 @@
 package dev;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import com.github.lalyos.jfiglet.FigletFont;
+
+
+public class App {
+	public static void main(String[] args) {
+		String asciiArt = FigletFont.convertOneLine("App");
+		System.out.println(asciiArt);
+	}
 }
